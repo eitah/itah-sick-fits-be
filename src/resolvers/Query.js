84b40c940a41f0c1a0fdc2fab3ce7a3ef3ce3551
Elijah Itah <elijahbit@gmail.com>
2019-03-10 14:12:54 -0500

    Breaking out backend from master project

---
+++ src/resolvers/Query.js
@@ -0,0 +1,13 @@
+const { forwardTo } = require('prisma-binding');
+
+
+const Query = {
+    items: forwardTo('db'),
+};
+
+module.exports = Query;
+
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
